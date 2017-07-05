a0b06380cbf5b561324857892f3eea6b5957d982
Laxman Singh <laxman-singh@users.noreply.github.com>
2017-07-05 16:41:17 +0530

    Add files via upload

--- DiffBackup.sh
+++ DiffBackup.sh
@@ -5,12 +5,21 @@
 ## SCRIPT BY : Laxman Singh <laxman.nrlm@gmail.com>
 ###########################################################################
 
-
+DAYS_TO_BACKUP=$1
 RED='\033[0;31m'
 GREEN='\033[32m'
 ORANGE='\033[33m'
-blue='\033[34m'
+BLUE='\033[34m'
 NC='\033[0m' # No Color
+
+re='^[0-9]+$'
+if [[ $# -eq 0 ]]; then
+	echo -e "${RED}USAGE:\tDiffBAckup.sh NO_OF_DAYS_TO_TAKE_BACKUP${NC}"; exit 1
+fi
+if [[ ${DAYS_TO_BACKUP} != [0-9]* ]] ; then
+   echo -e "${RED}error: Argument must be a number${NC}" >&2; exit 1
+fi
+
 
 echo -e "\n\n#################################################################\n \
 	${ORANGE}Welcome to Differential Backup Program${NC}\n\
@@ -18,27 +27,26 @@ ${GREEN} SCRIPT DEVELOPED BY : ${BLUE} Laxman Singh <laxman.nrlm@gmail.com>${NC}
 #################################################################\n"
 
 # find 20 days old files in ddugky directory and copy them to a temp location
-echo -e "${GREEN} copying 20 days old files from ddugky directory to ${RED}/tmp/DiffBackupDDUGKY_$(date +"%m-%d-%y")${NC}\n"
-mkdir -p /tmp/DiffBackupDDUGKY_$(date +"%m-%d-%y")
+echo -e "${GREEN} copying ${DAYS_TO_BACKUP} days old files from ddugky directory to ${RED}/tmp/DiffBackupDDUGKY_$(date +"%d-%m-%y")${NC}\n"
+mkdir -p /tmp/DiffBackupDDUGKY_$(date +"%d-%m-%y")
 cd /usr/local/tomcat7/webapps
-find ddugky -maxdepth 30 -mtime -20 -exec cp --parents '{}' /tmp/DiffBackupDDUGKY_$(date +"%m-%d-%y") ';'
+find ddugky -maxdepth 30 -mtime -${DAYS_TO_BACKUP} -exec cp --parents '{}' /tmp/DiffBackupDDUGKY_$(date +"%d-%m-%y") ';'
 echo -e "${GREEN} Done.......!!!! ${NC}\n"
 
 # find 20 days old files in FILEFOLDER directory and copy them to a temp location
-echo -e "${GREEN} copying 20 days old files from FILEFOLDER directory to ${RED}/tmp/DiffBackupDDUGKY_$(date +"%m-%d-%y")${NC}\n"
+echo -e "${GREEN} copying ${DAYS_TO_BACKUP} days old files from FILEFOLDER directory to ${RED}/tmp/DiffBackupDDUGKY_$(date +"%d-%m-%y")${NC}\n"
 #mkdir -p /tmp/DiffBackupDDUGKY_$(date +"%m-%d-%y")
-find /FileFolder -maxdepth 5 -mtime -20 -exec cp --parents '{}' /tmp/DiffBackupDDUGKY_$(date +"%m-%d-%y") ';' 
+find /FileFolder -maxdepth 5 -mtime -${DAYS_TO_BACKUP} -exec cp --parents '{}' /tmp/DiffBackupDDUGKY_$(date +"%d-%m-%y") ';'
 echo -e "${GREEN} Done.......!!!! ${NC}\n"
 
 # now compress files
 echo -e "${ORANGE}compressing files..... Please wait ${NC}"
 cd /tmp
-tar -czf DiffBackupDDUGKY_$(date +"%m-%d-%y").tar.gz DiffBackupDDUGKY_$(date +"%m-%d-%y")
+tar -czf DiffBackupDDUGKY_$(date +"%d-%m-%y")"last_"${DAYS_TO_BACKUP}"_Days".tar.gz DiffBackupDDUGKY_$(date +"%d-%m-%y")
 
-rm -rf DiffBackupDDUGKY_$(date +"%m-%d-%y")
+rm -rf DiffBackupDDUGKY_$(date +"%d-%m-%y")
 
 echo -e "${GREEN}compressing done ........................ !! \n ${RED}deleting temp files ...!!${NC}\n"
 
-echo -e "${ORANGE}############ Differential backup complete ######################\n ${GREEN}Thank You... \n ${GREEN} (^_^) ${ORANGE}(^_^)${NC}"
+echo -e "${ORANGE}############ Differential backup completed ######################${NC}\n\n"
 
-echo -e "${GREEN}\n########################################${GREEN} (^_^) ${ORANGE}(^_^)${NC} ###############################################${NC}\n\n"
